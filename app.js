5f296e67126f3a42285d465ca2b840a2b709df4d
Anna Jaśkiewicz <ania.jaskiewicz.2001@gmail.com>
2023-02-02 15:56:54 +0100

    Added tree making function

--- app.js
+++ app.js
@@ -6,5 +6,13 @@ const form = document.getElementById('settings');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('YOU CLICKED ME');
-})
+        let symbol = textInput.value;
+        let height = numberInput.valueAsNumber;
+        for (let i=1; i <= height; i++) {
+            let tree = document.createElement('div');
+            tree.textContent = '\u00A0'.repeat(height - i) + symbol.repeat(2 * i -1) ;
+            document.getElementById('treeBox').append(tree);
+            console.log(tree.textContent);
+        }
+    }
+)
