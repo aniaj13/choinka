5a3c768fd2b0ba541c05dd9d56a5d725eaf874f9
Anna Jaśkiewicz <ania.jaskiewicz.2001@gmail.com>
2023-02-10 10:32:38 +0100

    content refreshes each time you submit

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 let numberInput = document.getElementById('number');
 let textInput = document.getElementById('text');
 const submitBtn = document.getElementById('submitBtn');
-const treeBox = document.getElementById('treeBox');
 const form = document.getElementById('settings');
+const treeBox = document.getElementById('treeBox');
+
+
+
+function createTree() {
+    treeBox.innerText = '';
+    let symbol = textInput.value;
+    let height = numberInput.valueAsNumber;
+    for (let i=1; i <= height; i++) {
+        let tree = document.createElement('div');
+        tree.textContent = '\u00A0'.repeat(height - i) + symbol.repeat(2 * i -1) ;
+        document.getElementById('treeBox').append(tree);
+        console.log(tree.textContent);
+    }
+}
 
 form.addEventListener('submit', function (e) {
-    e.preventDefault();
-        let symbol = textInput.value;
-        let height = numberInput.valueAsNumber;
-        for (let i=1; i <= height; i++) {
-            let tree = document.createElement('div');
-            tree.textContent = '\u00A0'.repeat(height - i) + symbol.repeat(2 * i -1) ;
-            document.getElementById('treeBox').append(tree);
-            console.log(tree.textContent);
-        }
-    }
-)
+        e.preventDefault();
+        createTree();
+    })
