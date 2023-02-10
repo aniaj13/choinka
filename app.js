50c59a20b4872ad5f07f84b673940fd2da1ddb59
Anna Jaśkiewicz <ania.jaskiewicz.2001@gmail.com>
2023-02-10 11:07:04 +0100

    function now returns a single string

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let numberInput = document.getElementById('number');
 let textInput = document.getElementById('text');
-const submitBtn = document.getElementById('submitBtn');
 const form = document.getElementById('settings');
 const treeBox = document.getElementById('treeBox');
 
@@ -8,12 +7,12 @@ function createTree() {
     treeBox.innerText = '';
     let symbol = textInput.value;
     let height = numberInput.valueAsNumber;
+    let tree = '';
     for (let i=1; i <= height; i++) {
-        let tree = document.createElement('div');
-        tree.textContent = '\u00A0'.repeat(height - i) + symbol.repeat(2 * i -1) ;
-        document.getElementById('treeBox').append(tree);
-        console.log(tree.textContent);
+        tree += '\u00A0'.repeat(height - i) + symbol.repeat(2 * i -1) + '\n';
     }
+    treeBox.innerText = tree;
+    console.log(tree);
 }
 
 form.addEventListener('submit', function (e) {
