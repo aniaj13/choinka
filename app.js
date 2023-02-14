f086f250476b43746df98beb6d87342d5d4cc2c7
Anna Jaśkiewicz <ania.jaskiewicz.2001@gmail.com>
2023-02-14 11:21:46 +0100

    code review poprawki

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
-let numberInput = document.getElementById('number');
-let textInput = document.getElementById('text');
+let numberInput = document.getElementById('treeHeight');
+let textInput = document.getElementById('treeMaterial');
 const form = document.getElementById('settings');
 const treeBox = document.getElementById('treeBox');
-let pickedColor = document.getElementById('color');
+let pickedColor = document.getElementById('treeColor');
 
-let cancel = false;
 function validateForm() {
     if (textInput.value === '') {
         alert('Musisz wybrać materiał!');
-        cancel = true;
+        return false;
+    } else {
+        return true;
     }
 }
 function createTree() {
-    treeBox.innerText = '';
     let symbol = textInput.value;
     let height = numberInput.valueAsNumber;
     let tree = '';
-    validateForm();
-    if (cancel === true) {
-        cancel = false;
-    } else {
+
         tree += '\u00A0'.repeat(height - 1) + '*\n';
-    for (let i=1; i <= height; i++) {
-        tree += '\u00A0'.repeat(height - i) + symbol.repeat(2 * i -1) + '\n';
-    }
+        for (let i = 1; i <= height; i++) {
+            tree += '\u00A0'.repeat(height - i) + symbol.repeat(2 * i - 1) + '\n';
+        }
         if (height < 10) {
             tree += '\u00A0'.repeat(height - 1) + '|';
         } else {
-            tree += '\u00A0'.repeat(height - 2) + '|_|';}
-    treeBox.style.color = pickedColor.value;
-    treeBox.innerText = tree;
-    console.log(tree);
-}}
-
+            tree += '\u00A0'.repeat(height - 2) + '|_|';
+        }
+    return tree;
+}
 form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        createTree();
-        form.reset();
-    });
+    e.preventDefault();
+    treeBox.innerHTML = '';
+    if (validateForm() === true) {
+        let tree = createTree();
+        treeBox.innerText = tree;
+        treeBox.style.color = pickedColor.value;
+        console.log(tree);
+    }
+    form.reset();
+});
